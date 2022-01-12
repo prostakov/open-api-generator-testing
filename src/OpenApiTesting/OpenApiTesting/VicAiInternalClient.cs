de1ad33c191075fdc14146b3c179d30da213c146
Serhii Prostakov <serhii.prostrakov@gmail.com>
2022-01-12 13:38:21 +0200

    Add additional stuff

---
+++ src/OpenApiTesting/OpenApiTesting/VicAiInternalClient.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace OpenApiTesting
+{
+    public partial class VicAiInternalClient
+    {
+    }
+    
+    public class ExternalData : Dictionary<string, string>
+    {
+    }
+}
